60bf650e3f8e509b1ed85beeaa5ac9726d6e5c7f
ejaz <rehanrumi0786@gmail.com>
2023-05-19 17:10:42 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,13 @@
+
+$(document).ready(function(){
+    $(".owl-carousel").owlCarousel(
+        {
+            items:1,
+            loop:true,
+            margin:10,
+            autoplay:true,
+            autoplayTimeout:2500,
+            autoplayHoverPause:true
+        }
+    );
+  });
